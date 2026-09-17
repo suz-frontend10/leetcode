82fb94f95ce563f06d09a019c8ec21738e978d4b
Susanna Hebzibah . P <susanna.posupo@gmail.com>
2026-09-17 20:08:42 +0530

    Time: 1 ms (65.68%), Space: 20 MB (12.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
-
         for (int num : nums)
             freq[num]++;
-
         vector<vector<int>> bucket(nums.size() + 1);
-
         for (auto &p : freq) {
             bucket[p.second].push_back(p.first);
         }
-
         vector<int> ans;
-
         for (int i = nums.size(); i >= 1 && ans.size() < k; i--) {
             for (int num : bucket[i]) {
                 ans.push_back(num);
@@ -22,7 +17,6 @@ public:
                     return ans;
             }
         }
-
         return ans;
     }
 };
