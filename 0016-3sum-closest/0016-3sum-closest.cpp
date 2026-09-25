86b4d99fd8f666a72ac1116f0fb3e9e0039c7116
Susanna Hebzibah . P <susanna.posupo@gmail.com>
2026-09-25 21:58:09 +0530

    Time: 4 ms (94.91%), Space: 14.2 MB (15.03%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        int best = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(sum - target) < abs(best - target)) {
+                    best = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return target;
+                }
+            }
+        }
+
+        return best;
+    }
+};
