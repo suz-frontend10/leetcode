345b9f217d3f9c664e039e8b96a3d2ad4a790ba4
Susanna Hebzibah . P <susanna.posupo@gmail.com>
2026-09-25 21:59:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (97.57%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        long long dvd = llabs((long long)dividend);
+        long long dvs = llabs((long long)divisor);
+
+        long long quotient = 0;
+
+        while (dvd >= dvs) {
+            long long temp = dvs;
+            long long multiple = 1;
+
+            while ((temp << 1) <= dvd) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            quotient += multiple;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            quotient = -quotient;
+
+        return (int)quotient;
+    }
+};
